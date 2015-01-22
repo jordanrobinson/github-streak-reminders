71c467d7fd47d148d6ea3474171b6f3466ff9c90
Jordan Robinson <hypahnova@gmail.com>
2015-01-22 22:10:43 +0000

    some small updates

--- app/src/main/java/uk/co/jordanrobinson/githubstreakreminders/LoginActivity.java
+++ app/src/main/java/uk/co/jordanrobinson/githubstreakreminders/LoginActivity.java
@@ -220,14 +220,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
                 return false;
             }
 
-//            for (String credential : DUMMY_CREDENTIALS) {
-//                String[] pieces = credential.split(":");
-//                if (pieces[0].equals(username) || true) {
-//                    // Account exists, return true if the password matches.
-//                    return pieces[1].equals(username) || true;
-//                }
-//            }
-
             // TODO: register the new account here.
             return true;
         }
